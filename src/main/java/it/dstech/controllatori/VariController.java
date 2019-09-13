1a01340a0c9b65cb5acee6927cdfc576b9b4296a
almanaccogratuito <simonemunafo@libero.it>
2019-09-13 17:51:22 +0200

    back end controlli a parte terminato ;)

--- src/main/java/it/dstech/controllatori/VariController.java
+++ src/main/java/it/dstech/controllatori/VariController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.dstech.databasici.Db;
+import it.dstech.stato.Citta;
 import it.dstech.stato.Stati;
 
 @Controller
@@ -36,8 +37,34 @@ public class VariController
 	{
 		List <Stati> stati = new ArrayList<Stati>();
 		stati.addAll(conn.prendiStati(continente));
+		model.addAttribute("message", message);
 		model.addAttribute("stati", stati);
 		return "stato";
+	}
+	
+	@PostMapping("citta")
+	public String citta (@RequestParam(name="stato")String codiceStato , Model model) throws ClassNotFoundException, SQLException
+	{
+		List<Citta> city = new ArrayList <Citta>();
+		//questo metodo ti fa tornare la lista di tutte le citta
+		city.addAll(conn.prendiCitta(codiceStato));
+		model.addAttribute(codiceStato);
+		model.addAttribute("message", message);
+		model.addAttribute("citta", city);
+		return "citta";
+	}
+	
+	@PostMapping("nuovacitta")
+	public String nuovacitta (@RequestParam(name="")String nomeCitta , @RequestParam(name="")String distretto, @RequestParam(name="")String codiceStato , Model model ) throws ClassNotFoundException, SQLException
+	{
+		conn.inserisciNuovaCitta(nomeCitta , distretto, codiceStato);
+		message = "citta' aggiunta correttamente, bentornati Marco e Simone , volete aggiungere una nuova citta' ? divertitevi ! :)" ;
+		model.addAttribute("message" , message);
+		
+		List <String> continenti = new ArrayList<String>();
+		continenti.addAll(conn.prendiContinenti());		
+		model.addAttribute("continenti", continenti);
+		return "continente";
 	}
 	
 
