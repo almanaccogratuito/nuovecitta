ec5645452d1e976a675d660b82a923d06ab5b7b9
MarcoGuarnera <marco.guarnera@hotmail.com>
2019-09-13 14:51:44 +0200

    Commit con modifiche varie e front end pagina1

--- src/main/java/it/dstech/controllatori/VariController.java
+++ src/main/java/it/dstech/controllatori/VariController.java
@@ -16,18 +16,22 @@ import it.dstech.stato.Stati;
 @Controller
 public class VariController
 {
-	@Value
+	@Value("${welcome.message}")
 	private String message;
 	
 	Db conn = new Db();
-	@PostMapping("/")
-	public String continenti (Model model)
+	@GetMapping("/")
+	public String continenti (Model model) throws ClassNotFoundException, SQLException
 	{
 		List <String> continenti = new ArrayList<String>();
 		continenti.addAll(conn.prendiContinenti());
+		for (int i = 0; i < continenti.size(); i++) {
+			System.out.println(continenti.get(i));
+			
+		}
 		model.addAttribute("continenti", continenti);
 		model.addAttribute("message", message);
-		return "continent";
+		return "continente";
 	}
 	
 	
